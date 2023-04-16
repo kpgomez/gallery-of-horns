ae4d35b58f193f2fc57bb3389bd3aad39225fe61
Kiengchay Gomez <kphoutthavong@gmail.com>
2023-04-16 01:04:40 -0500

    added Main, HornedBeast, Footer, and Header component functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+//brings in the React Library
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Main from "./Main";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//React.Component is the parent and App receives all of the parent's functionality
+class App extends React.Component { //class name matches the name of the file
+  render() { //required function for every class
+    return ( //every render method has a return()
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+    )    
+  }
 }
 
-export default App;
+//defines what you want to export
+export default App
